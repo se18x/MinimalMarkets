ec3877bcf27810ae2673dcacc36b6e651616276f
se18x <shashanke7y@gmail.com>
2024-08-19 10:16:04 -0400

    create navbar and also blank input field

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import "./App.css";
+import Logo from "./minimal-markets-logo.svg";
 
 function App() {
+  const findStocks = () => {
+    console.log("hello");
+  };
   return (
-    <div>
-      <header>insert extremely volatile company here</header>
+    <div className="Main-container">
+      <div className="Navbar-container">
+        <nav className="Navbar">
+          <div className="brand">
+            <img className="Logo" src={Logo} alt="Minimal Markets Logo" />
+            <h1>Minimal Markets</h1>
+          </div>
+          <div className="account">
+            <img
+              className="profile"
+              src="https://s28126.pcdn.co/blogs/ask-experian/wp-content/uploads/what-are-different-types-of-stocks.jpg.optimal.jpg"
+              alt="User Profile Picture"
+            />
+          </div>
+        </nav>
+      </div>
+      <div className="Wrapper">
+        <div className="Stock-search-container">
+          <div className="Stock-search">
+            <input
+              type="text"
+              placeholder="What's on your mind?"
+              className="Stock-search-input"
+            />
+            <button onClick={findStocks} className="Stock-search-btn">
+              Find
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
